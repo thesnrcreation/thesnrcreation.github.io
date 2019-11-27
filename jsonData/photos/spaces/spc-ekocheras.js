69748fa4d0d153c341a605def13285b3b3c6ca35
thesnrcreation <thesnrcreation@gmail.com>
2019-11-27 17:09:35 +0800

    rename path and add wedding jhaz Aug 2019

--- jsonData/photos/spaces/spc-ekocheras.js
+++ jsonData/photos/spaces/spc-ekocheras.js
@@ -1,54 +1,54 @@
 var portfolio_photo = {
     "galleryObj": [{
         "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-01.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-01.jpg",
+        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-01.jpg",
+        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-01.jpg",
         "alt_pic": "Interior Photo 01"
     }, {
         "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-02.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-02.jpg",
+        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-02.jpg",
+        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-02.jpg",
         "alt_pic": "Interior Photo 02"
 
     }, {
         "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-03.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-03.jpg",
+        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-03.jpg",
+        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-03.jpg",
         "alt_pic": "Interior Photo 03"
     }, {
         "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-04.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-04.jpg",
+        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-04.jpg",
+        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-04.jpg",
         "alt_pic": "Interior Photo 04"
     }, {
         "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-05.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-05.jpg",
+        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-05.jpg",
+        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-05.jpg",
         "alt_pic": "Interior Photo 05"
     }, {
         "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-06.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-06.jpg",
+        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-06.jpg",
+        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-06.jpg",
         "alt_pic": "Interior Photo 06"
     }, {
         "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-07.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-07.jpg",
+        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-07.jpg",
+        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-07.jpg",
         "alt_pic": "Interior Photo 07"
     }, {
         "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-08.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-08.jpg",
+        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-08.jpg",
+        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-08.jpg",
         "alt_pic": "Interior Photo 08"
     }, {
         "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-09.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-09.jpg",
+        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-09.jpg",
+        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-09.jpg",
         "alt_pic": "Interior Photo 09"
     }, {
         "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-10.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras/portfolio/eko-10.jpg",
+        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-10.jpg",
+        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-10.jpg",
         "alt_pic": "Interior Photo 10"
     }]
 }
---
+++ jsonData/photos/wedding/wed-jhaz-2018.js
@@ -0,0 +1,203 @@
+var portfolio_photo = {
+    "galleryObj": [{
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-01.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-01.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 01"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-02.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-02.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 02"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-03.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-03.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 03"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-04.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-04.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 04"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-05.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-05.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 05"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-06.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-06.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 06"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-07.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-07.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 07"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-08.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-08.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 08"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-09.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-09.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 09"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-10.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-10.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 10"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-11.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-11.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 11"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-12.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-12.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 12"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-13.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-13.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 13"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-14.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-14.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 14"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-15.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-15.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 15"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-16.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-16.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 16"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-17.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-17.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 17"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-18.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-18.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 18"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-19.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-19.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 19"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-20.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-20.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 20"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-21.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-21.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 21"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-23.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-23.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 23"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-24.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-24.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 24"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-25.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-25.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 25"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-26.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-26.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 25"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-27.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-27.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 26"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-28.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-28.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 28"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-29.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-29.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 29"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-30.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-30.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 30"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-31.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-31.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 31"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-32.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-32.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 32"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-33.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-33.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 33"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-34.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-34.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 34"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-35.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-35.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 35"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-36.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-36.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 36"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-37.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-37.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 37"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-38.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-38.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 38"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-39.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-39.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 39"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-40.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-40.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 40"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-41.jpg",
+        "normal_pic": "assets/images/photos/wedding/jhaz-aug-2018/portfolio/jhaz-41.jpg",
+        "alt_pic": "Actual Wedding - Jaha & Azweina 41"
+    }]
+}
