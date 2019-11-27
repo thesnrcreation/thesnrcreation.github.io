53337d863cf66f11fb04abe4539c578069738b2e
thesnrcreation <thesnrcreation@gmail.com>
2019-11-27 12:45:19 +0800

    update render video gallery

---
+++ js/rendVideoGallery.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    "use strict";
+    renderVideoGallery();
+});
+
+function renderVideoGallery() {
+    $("#portfolio-video-container").html(videoGallery(portfolio_video));
+}
+
+function videoGallery(portfolio_video) {
+    var htmlGallery = "";
+    var galleryData;
+    for (var i = 0, n = portfolio_video.galleryObj.length; i < n; i++) {
+        galleryData = portfolio_video.galleryObj[i];
+        if (galleryData) {
+            htmlGallery += '<div class="portfolio-item col-12 col-md-4">' +
+                '<div class="embed-responsive embed-responsive-16by9 js-videoWrapper mb-2">' +
+                '<iframe class="embed-responsive-item js-videoIframe" src="" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" data-src="' + galleryData.video + '" allowfullscreen></iframe>' +
+                '<div class="videoPoster js-videoPoster" style="background-image:url(' + galleryData.thumbnail + ');">' +
+                '<div class="videoPoster_overlay_hover">' +
+                '<i class="video_play fa fa-play"></i>' +
+                '</div></div></div>' +
+                '<div class="video-text-header">' + galleryData.desc +
+                '</div>' +
+                '</div>';
+        } else {
+            console.log("galleryData >> EMPTY !!")
+        }
+    }
+    return htmlGallery;
+}
---
+++ jsonData/videos/portfolio-video.js
@@ -0,0 +1,27 @@
+var portfolio_video = {
+    "galleryObj": [{
+        "video": "https://www.youtube.com/embed/1d-Jf11IfG8?autoplay=1",
+        "thumbnail": "http://i2.wp.com/www.cgmeetup.net/home/wp-content/uploads/2015/05/Avengers-Age-of-Ultron-UI-Reel-1.jpg",
+        "desc": "DNA 32"
+    }, {
+        "video": "https://www.youtube.com/embed/FpaSrU-GWOU?autoplay=1",
+        "thumbnail": "http://i2.wp.com/www.cgmeetup.net/home/wp-content/uploads/2015/05/Avengers-Age-of-Ultron-UI-Reel-1.jpg",
+        "desc": "F&N CNY"
+    }, {
+        "video": "https://www.youtube.com/embed/g-MSTWBGdTo?autoplay=1",
+        "thumbnail": "http://i2.wp.com/www.cgmeetup.net/home/wp-content/uploads/2015/05/Avengers-Age-of-Ultron-UI-Reel-1.jpg",
+        "desc": "MARINA ISLAND | Theme park | Promo"
+    }, {
+        "video": "https://www.youtube.com/embed/FxD4NK33TE0?autoplay=1",
+        "thumbnail": "http://i2.wp.com/www.cgmeetup.net/home/wp-content/uploads/2015/05/Avengers-Age-of-Ultron-UI-Reel-1.jpg",
+        "desc": "Golf finale | Carlsberg"
+    }, {
+        "video": "https://www.youtube.com/embed/fX_i1W5iIrA?autoplay=1",
+        "thumbnail": "http://i2.wp.com/www.cgmeetup.net/home/wp-content/uploads/2015/05/Avengers-Age-of-Ultron-UI-Reel-1.jpg",
+        "desc": "Top 10 charity | Carlsberg | Event"
+    }, {
+        "video": "https://www.youtube.com/embed/moRkBOddtzE?autoplay=1",
+        "thumbnail": "http://i2.wp.com/www.cgmeetup.net/home/wp-content/uploads/2015/05/Avengers-Age-of-Ultron-UI-Reel-1.jpg",
+        "desc": "BE launching | Event"
+    }]
+}
