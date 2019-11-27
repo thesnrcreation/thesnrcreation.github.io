555c2431371c8d8f683550b7bd6534613508329d
thesnrcreation <thesnrcreation@gmail.com>
2019-11-27 11:53:51 +0800

    upload new Render Img Gallery

--- jsonData/spaces/spc-ekocheras.js
+++ jsonData/spaces/spc-ekocheras.js
@@ -1,36 +1,3 @@
-$(document).ready(function () {
-    "use strict";
-    renderImgGallery();
-});
-
-function renderImgGallery() {
-    $("#portfolio-container").html(imgGallery(portfolio_photo));
-}
-
-function imgGallery(portfolio_photo) {
-    var htmlGallery = "";
-    var galleryData;
-    for (var i = 0, n = portfolio_photo.galleryObj.length; i < n; i++) {
-        galleryData = portfolio_photo.galleryObj[i];
-        if (galleryData) {
-            htmlGallery += '<div class="portfolio-item ' + galleryData.colSize + '">' +
-                '<a href="' + galleryData.fullsize_pic + '">' +
-                '<div class="portfolio-content">' +
-                '<div class="portfolio-img-content">' +
-                '<img src="' + galleryData.normal_pic + '" alt="' + galleryData.alt_pic + '">' +
-                '</div>' +
-                '</div>' +
-                '</a>' +
-                '</div>';
-        } else {
-            console.log("galleryData >> EMPTY !!");
-        }
-    }
-    return htmlGallery;
-}
-
-
-// Sample Json Portfolio Photo Gallery
 var portfolio_photo = {
     "galleryObj": [{
         "colSize": "col-md",
