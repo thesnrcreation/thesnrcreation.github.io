22a5e407066f15c31ec701389fff3e9fb754ece0
thesnrcreation <thesnrcreation@gmail.com>
2019-12-06 14:31:41 +0800

    Add 2 video portfolio - jymt prewedding - empower women interview

--- jsonData/photos/spaces/spc-ekocheras.js
+++ jsonData/photos/spaces/spc-ekocheras.js
@@ -3,52 +3,52 @@ var portfolio_photo = {
         "colSize": "col-md",
         "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-01.jpg",
         "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-01.jpg",
-        "alt_pic": "Interior Photo 01"
+        "alt_pic": "Interior - Eko Cheras 01"
     }, {
         "colSize": "col-md",
         "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-02.jpg",
         "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-02.jpg",
-        "alt_pic": "Interior Photo 02"
+        "alt_pic": "Interior - Eko Cheras 02"
 
     }, {
         "colSize": "col-md",
         "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-03.jpg",
         "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-03.jpg",
-        "alt_pic": "Interior Photo 03"
+        "alt_pic": "Interior - Eko Cheras 03"
     }, {
         "colSize": "col-md",
         "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-04.jpg",
         "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-04.jpg",
-        "alt_pic": "Interior Photo 04"
+        "alt_pic": "Interior - Eko Cheras 04"
     }, {
         "colSize": "col-md",
         "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-05.jpg",
         "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-05.jpg",
-        "alt_pic": "Interior Photo 05"
+        "alt_pic": "Interior - Eko Cheras 05"
     }, {
         "colSize": "col-md",
         "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-06.jpg",
         "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-06.jpg",
-        "alt_pic": "Interior Photo 06"
+        "alt_pic": "Interior - Eko Cheras 06"
     }, {
         "colSize": "col-md",
         "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-07.jpg",
         "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-07.jpg",
-        "alt_pic": "Interior Photo 07"
+        "alt_pic": "Interior - Eko Cheras 07"
     }, {
         "colSize": "col-md",
         "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-08.jpg",
         "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-08.jpg",
-        "alt_pic": "Interior Photo 08"
+        "alt_pic": "Interior - Eko Cheras 08"
     }, {
         "colSize": "col-md",
         "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-09.jpg",
         "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-09.jpg",
-        "alt_pic": "Interior Photo 09"
-    }, {
+        "alt_pic": "Interior - Eko Cheras 09"
+    }, { 
         "colSize": "col-md",
         "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-10.jpg",
         "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-10.jpg",
-        "alt_pic": "Interior Photo 10"
+        "alt_pic": "Interior - Eko Cheras 10"
     }]
 }
--- jsonData/videos/portfolio-video.js
+++ jsonData/videos/portfolio-video.js
@@ -1,5 +1,13 @@
 var portfolio_video = {
     "galleryObj": [{
+        "video": "https://www.youtube.com/embed/fjzkm2l1g1s?autoplay=1",
+        "thumbnail": "assets/images/photos/thumbnail/jymt-thumbnail.png",
+        "desc": "Jin Yong & Mui Ting - Pre-Wedding Slideshow"
+    }, {
+        "video": " https://www.youtube.com/embed/nW7H7VjuB3g?autoplay=1",
+        "thumbnail": "assets/images/photos/thumbnail/empower-women.png",
+        "desc": "Empower Women Beauty - Interview"
+    }, {
         "video": "https://www.youtube.com/embed/1d-Jf11IfG8?autoplay=1",
         "thumbnail": "http://i2.wp.com/www.cgmeetup.net/home/wp-content/uploads/2015/05/Avengers-Age-of-Ultron-UI-Reel-1.jpg",
         "desc": "DNA 32"
