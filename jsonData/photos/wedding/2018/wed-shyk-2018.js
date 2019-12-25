b935a54e0da119ba1464b1e2130e27a00e1a1aa2
thesnrcreation <thesnrcreation@gmail.com>
2019-12-26 00:59:32 +0800

    upload new wedding portfolio sjyk 2018

---
+++ jsonData/photos/wedding/2018/wed-shyk-2018.js
@@ -0,0 +1,238 @@
+var portfolio_photo = {
+    "galleryObj": [{
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-01.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-01.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 01"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-02.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-02.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 02"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-03.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-03.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 03"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-04.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-04.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 04"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-05.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-05.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 05"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-06.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-06.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 06"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-07.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-07.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 07"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-09.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-09.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 09"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-10.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-10.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 10"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-11.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-11.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 11"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-12.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-12.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 12"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-13.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-13.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 13"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-14.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-14.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 14"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-15.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-15.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 15"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-16.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-16.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 16"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-17.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-17.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 17"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-18.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-18.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 18"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-19.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-19.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 19"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-20.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-20.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 20"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-21.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-21.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 21"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-23.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-23.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 23"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-24.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-24.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 24"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-25.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-25.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 25"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-26.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-26.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 26"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-27.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-27.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 27"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-28.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-28.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 28"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-29.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-29.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 29"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-30.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-30.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 30"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-31.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-31.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 31"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-32.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-32.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 32"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-33.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-33.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 33"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-34.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-34.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 34"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-35.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-35.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 35"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-36.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-36.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 36"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-37.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-37.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 37"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-38.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-38.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 38"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-39.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-39.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 39"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-40.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-40.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 40"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-41.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-41.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 41"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-42.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-42.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 42"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-43.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-43.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 43"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-44.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-44.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 44"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-45.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-45.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 45"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-46.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-46.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 46"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-47.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-47.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 47"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-48.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-48.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 48"
+    }, {
+        "colSize": "col-md",
+        "fullsize_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-49.jpg",
+        "normal_pic": "assets/images/photos/wedding/shyk-dec-2018/portfolio/shyk-49.jpg",
+        "alt_pic": "Actual Wedding - Shen Hwa & Yoke King 49"
+    }]
+}
