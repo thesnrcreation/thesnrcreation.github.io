9eceaefe4dbe5fbe3f991ed2f9ace4c493a7c747
thesnrcreation <thesnrcreation@gmail.com>
2020-04-29 18:43:38 +0800

    rename and add new file 2020

--- jsonData/photos/spaces/2019/spc-ekocheras.js
+++ jsonData/photos/spaces/2019/spc-ekocheras.js
@@ -1,53 +1,53 @@
 var portfolio_photo = {
     "galleryObj": [{
-        "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-01.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-01.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-01.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-01.jpg",
         "alt_pic": "Interior - Eko Cheras 01"
     }, {
-        "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-02.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-02.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-02.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-02.jpg",
         "alt_pic": "Interior - Eko Cheras 02"
     }, {
-        "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-03.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-03.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-03.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-03.jpg",
         "alt_pic": "Interior - Eko Cheras 03"
     }, {
-        "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-04.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-04.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-04.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-04.jpg",
         "alt_pic": "Interior - Eko Cheras 04"
     }, {
-        "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-05.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-05.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-05.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-05.jpg",
         "alt_pic": "Interior - Eko Cheras 05"
     }, {
-        "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-06.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-06.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-06.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-06.jpg",
         "alt_pic": "Interior - Eko Cheras 06"
     }, {
-        "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-07.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-07.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-07.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-07.jpg",
         "alt_pic": "Interior - Eko Cheras 07"
     }, {
-        "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-08.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-08.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-08.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-08.jpg",
         "alt_pic": "Interior - Eko Cheras 08"
     }, {
-        "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-09.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-09.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-09.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-09.jpg",
         "alt_pic": "Interior - Eko Cheras 09"
     }, {
-        "colSize": "col-md",
-        "fullsize_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-10.jpg",
-        "normal_pic": "assets/images/photos/spaces/ekocheras-2019/portfolio/eko-10.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-10.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-10.jpg",
         "alt_pic": "Interior - Eko Cheras 10"
     }]
 }
--- jsonData/videos/portfolio-video.js
+++ jsonData/videos/portfolio-video.js
@@ -1,47 +1,59 @@
 var portfolio_video = {
     "galleryObj": [{
-        "video": "https://www.youtube.com/embed/-mP8Epf1T4I?autoplay=1",
-        "thumbnail": "assets/images/photos/thumbnail/langtengah-thumbnail.jpg",
-        "desc": "THINGS that I picked up at Lang Tengah Island"
+        "video": "https://www.youtube.com/embed/wZp3C4F-cx0?autoplay=1",
+        "thumbnail": "assets/images/photos/thumbnail/les-3.png",
+        "desc": "Le Paradis' Wen with Joyce Koid - Commercial"
     }, {
-        "video": "https://www.youtube.com/embed/fjzkm2l1g1s?autoplay=1",
-        "thumbnail": "assets/images/photos/thumbnail/jymt-thumbnail.png",
-        "desc": "Jin Yong & Mui Ting - Pre-Wedding Slideshow"
-    }, {
-        "video": " https://www.youtube.com/embed/nW7H7VjuB3g?autoplay=1",
-        "thumbnail": "assets/images/photos/thumbnail/empower-women-thumbnail.png",
-        "desc": "Empower Women Beauty - Interview"
-    }, {
-        "video": " https://www.youtube.com/embed/j6pa8iI1jiI?autoplay=1",
-        "thumbnail": "assets/images/photos/thumbnail/trx-thumbnail.jpg",
-        "desc": "City Explore"
+        "video": "https://www.youtube.com/embed/LL_uWSrXKpI?autoplay=1",
+        "thumbnail": "assets/images/photos/thumbnail/spade.png",
+        "desc": "Spade Club | Modeling Cinematic | BTS"
     }, {
         "video": " https://www.youtube.com/embed/BciAx0g2qdo?autoplay=1",
         "thumbnail": "assets/images/photos/thumbnail/empro-thumbnail.jpg",
         "desc": "Empro - Commercial"
     }, {
-        "video": "https://www.youtube.com/embed/1d-Jf11IfG8?autoplay=1",
-        "thumbnail": "http://i2.wp.com/www.cgmeetup.net/home/wp-content/uploads/2015/05/Avengers-Age-of-Ultron-UI-Reel-1.jpg",
-        "desc": "DNA 32"
+        "video": " https://www.youtube.com/embed/nW7H7VjuB3g?autoplay=1",
+        "thumbnail": "assets/images/photos/thumbnail/empower-women-thumbnail.png",
+        "desc": "Empower Women Beauty - Commercial (Interview)"
     }, {
-        "video": "https://www.youtube.com/embed/FpaSrU-GWOU?autoplay=1",
-        "thumbnail": "http://i2.wp.com/www.cgmeetup.net/home/wp-content/uploads/2015/05/Avengers-Age-of-Ultron-UI-Reel-1.jpg",
-        "desc": "F&N CNY"
+        "video": "https://www.youtube.com/embed/-mP8Epf1T4I?autoplay=1",
+        "thumbnail": "assets/images/photos/thumbnail/langtengah-thumbnail.jpg",
+        "desc": "Plastic Awareness - Commercial"
     }, {
         "video": "https://www.youtube.com/embed/g-MSTWBGdTo?autoplay=1",
-        "thumbnail": "http://i2.wp.com/www.cgmeetup.net/home/wp-content/uploads/2015/05/Avengers-Age-of-Ultron-UI-Reel-1.jpg",
-        "desc": "MARINA ISLAND | Theme park | Promo"
+        "thumbnail": "assets/images/photos/thumbnail/marina-island.png",
+        "desc": "MARINA ISLAND | Theme park - Commercial"
+    }, {
+        "video": "https://www.youtube.com/embed/iJAxUBs3qqw",
+        "thumbnail": "assets/images/photos/thumbnail/spade.png",
+        "desc": "Portrait - Ivin"
+    }, {
+        "video": "https://www.youtube.com/embed/fjzkm2l1g1s?autoplay=1",
+        "thumbnail": "assets/images/photos/thumbnail/jymt-thumbnail.png",
+        "desc": "Jin Yong & Mui Ting - Pre-Wedding Slideshow"
+    }, {
+        "video": " https://www.youtube.com/embed/j6pa8iI1jiI?autoplay=1",
+        "thumbnail": "assets/images/photos/thumbnail/trx-thumbnail.jpg",
+        "desc": "City Explore"
     }, {
         "video": "https://www.youtube.com/embed/FxD4NK33TE0?autoplay=1",
-        "thumbnail": "http://i2.wp.com/www.cgmeetup.net/home/wp-content/uploads/2015/05/Avengers-Age-of-Ultron-UI-Reel-1.jpg",
-        "desc": "Golf finale | Carlsberg"
+        "thumbnail": "assets/images/photos/thumbnail/golf-finale.png",
+        "desc": "Golf finale | Carlsberg - Event"
+    }, {
+        "video": "https://www.youtube.com/embed/1d-Jf11IfG8?autoplay=1",
+        "thumbnail": "assets/images/photos/thumbnail/dna-thumbnail.png",
+        "desc": "DNA 32 - Event"
+    }, {
+        "video": "https://www.youtube.com/embed/FpaSrU-GWOU?autoplay=1",
+        "thumbnail": "assets/images/photos/thumbnail/f_n-thumbnail.png",
+        "desc": "F&N CNY - Event"
     }, {
         "video": "https://www.youtube.com/embed/fX_i1W5iIrA?autoplay=1",
-        "thumbnail": "http://i2.wp.com/www.cgmeetup.net/home/wp-content/uploads/2015/05/Avengers-Age-of-Ultron-UI-Reel-1.jpg",
-        "desc": "Top 10 charity | Carlsberg | Event"
+        "thumbnail": "assets/images/photos/thumbnail/top-10.png",
+        "desc": "Top 10 charity | Carlsberg - Event"
     }, {
         "video": "https://www.youtube.com/embed/moRkBOddtzE?autoplay=1",
-        "thumbnail": "http://i2.wp.com/www.cgmeetup.net/home/wp-content/uploads/2015/05/Avengers-Age-of-Ultron-UI-Reel-1.jpg",
-        "desc": "BE launching | Event"
+        "thumbnail": "assets/images/photos/thumbnail/BE-launch.png",
+        "desc": "BE launching - Event"
     }]
 }
