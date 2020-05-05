0c56dd8876d381f8b9dcea77702d1be6e3ce9420
thesnrcreation <thesnrcreation@gmail.com>
2020-05-06 00:13:03 +0800

    fix puma mobile view col

--- jsonData/photos/commercial/2019/cm-puma-training.js
+++ jsonData/photos/commercial/2019/cm-puma-training.js
@@ -1,16 +1,16 @@
 var portfolio_photo = {
     "galleryObj": [{
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-1.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-1.jpg",
         "alt_pic": "Puma Football Training Kit 01"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-2.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-2.jpg",
         "alt_pic": "Puma Football Training Kit 02"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-3.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-3.jpg",
         "alt_pic": "Puma Football Training Kit 03"
@@ -20,17 +20,17 @@ var portfolio_photo = {
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-4.jpg",
         "alt_pic": "Puma Football Training Kit 04"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-6.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-6.jpg",
         "alt_pic": "Puma Football Training Kit 06"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-7.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-7.jpg",
         "alt_pic": "Puma Football Training Kit 07"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-8.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-8.jpg",
         "alt_pic": "Puma Football Training Kit 08"
@@ -50,17 +50,17 @@ var portfolio_photo = {
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-11.jpg",
         "alt_pic": "Puma Football Training Kit 11"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-12.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-12.jpg",
         "alt_pic": "Puma Football Training Kit 12"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-13.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-13.jpg",
         "alt_pic": "Puma Football Training Kit 13"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-14.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-14.jpg",
         "alt_pic": "Puma Football Training Kit 14"
@@ -80,17 +80,17 @@ var portfolio_photo = {
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-17.jpg",
         "alt_pic": "Puma Football Training Kit 17"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-18.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-18.jpg",
         "alt_pic": "Puma Football Training Kit 18"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-19.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-19.jpg",
         "alt_pic": "Puma Football Training Kit 19"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-20.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-20.jpg",
         "alt_pic": "Puma Football Training Kit 20"
@@ -100,17 +100,17 @@ var portfolio_photo = {
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-21.jpg",
         "alt_pic": "Puma Football Training Kit 21"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-22.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-22.jpg",
         "alt_pic": "Puma Football Training Kit 22"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-23.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-23.jpg",
         "alt_pic": "Puma Football Training Kit 23"
     }, {
-        "colSize": "col-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-24.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/aug/puma-training/portfolio/puma-24.jpg",
         "alt_pic": "Puma Football Training Kit 24"
