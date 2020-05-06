e525fe97aa8e8574f4b8311488e1bccbfb44b7ea
thesnrcreation <thesnrcreation@gmail.com>
2020-05-06 23:42:24 +0800

    Upload new travel photo portfolio - Lang Tengah | Sep 2019

---
+++ jsonData/photos/travel/2019/travel-langtengah.js
@@ -0,0 +1,78 @@
+var portfolio_photo = {
+    "galleryObj": [{
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-01.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-01.jpg",
+        "alt_pic": "Terengganu Lang Tengah 01"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-02.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-02.jpg",
+        "alt_pic": "Terengganu Lang Tengah 02"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-03.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-03.jpg",
+        "alt_pic": "Terengganu Lang Tengah 03"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-04.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-04.jpg",
+        "alt_pic": "Terengganu Lang Tengah 04"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-05.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-05.jpg",
+        "alt_pic": "Terengganu Lang Tengah 05"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-06.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-06.jpg",
+        "alt_pic": "Terengganu Lang Tengah 06"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-07.png",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-07.jpg",
+        "alt_pic": "Terengganu Lang Tengah 07"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-08.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-08.jpg",
+        "alt_pic": "Terengganu Lang Tengah 08"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-09.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-09.jpg",
+        "alt_pic": "Terengganu Lang Tengah 9"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-10.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-10.jpg",
+        "alt_pic": "Terengganu Lang Tengah 10"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-11.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/lgth-11.jpg",
+        "alt_pic": "Terengganu Lang Tengah 12"
+    },{
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/group-01.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/group-01.jpg",
+        "alt_pic": "Terengganu Lang Tengah 01-1"
+    },{
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/group-02.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/group-02.jpg",
+        "alt_pic": "Terengganu Lang Tengah 01-2"
+    },{
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/group-03.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/group-03.jpg",
+        "alt_pic": "Terengganu Lang Tengah 01-3"
+    },{
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/group-04.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/lang-tengah/portfolio/group-04.jpg",
+        "alt_pic": "Terengganu Lang Tengah 01-4"
+    }]
+}
