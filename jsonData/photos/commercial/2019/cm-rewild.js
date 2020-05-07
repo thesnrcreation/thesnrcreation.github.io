9779f8ca4e5f7bd7d799d4b5bc48829f8a60e790
thesnrcreation <thesnrcreation@gmail.com>
2020-05-07 23:03:47 +0800

    upload new photo portfolio - Rewild March 2019

---
+++ jsonData/photos/commercial/2019/cm-rewild.js
@@ -0,0 +1,53 @@
+var portfolio_photo = {
+    "galleryObj": [{
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-6.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-6.jpg",
+        "alt_pic": "Rewild - Reusable Straw 06"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-2.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-2.jpg",
+        "alt_pic": "Rewild - Reusable Straw 02"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-3.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-3.jpg",
+        "alt_pic": "Rewild - Reusable Straw 03"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-1.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-1.jpg",
+        "alt_pic": "Rewild - Reusable Straw 01"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-4.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-4.jpg",
+        "alt_pic": "Rewild - Reusable Straw 04"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-7.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-7.jpg",
+        "alt_pic": "Rewild - Reusable Straw 07"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-8.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-8.jpg",
+        "alt_pic": "Rewild - Reusable Straw 08"
+    }, {
+        "colSize": "col-4",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-9.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-9.jpg",
+        "alt_pic": "Rewild - Reusable Straw 09"
+    }, {
+        "colSize": "col-4",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-10.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-10.jpg",
+        "alt_pic": "Rewild - Reusable Straw 10"
+    }, {
+        "colSize": "col-4",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-11.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-11.jpg",
+        "alt_pic": "Rewild - Reusable Straw 11"
+    }]
+}
