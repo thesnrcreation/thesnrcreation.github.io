3b22197770d531abfdfc9198c5001fe3a1337452
thesnrcreation <thesnrcreation@gmail.com>
2020-05-07 22:31:14 +0800

    upload new commercial photo portfolio - regenie skin care feb 2019

---
+++ jsonData/photos/commercial/2019/cm-regeine-skincare.js
@@ -0,0 +1,53 @@
+var portfolio_photo = {
+    "galleryObj": [{
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-1.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-1.jpg",
+        "alt_pic": "Regenie - Skin Care Product 01"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-2.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-2.jpg",
+        "alt_pic": "Regenie - Skin Care Product 02"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-3.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-3.jpg",
+        "alt_pic": "Regenie - Skin Care Product 03"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-4.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-4.jpg",
+        "alt_pic": "Regenie - Skin Care Product 04"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-6.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-6.jpg",
+        "alt_pic": "Regenie - Skin Care Product 06"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-7.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-7.jpg",
+        "alt_pic": "Regenie - Skin Care Product 07"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-8.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-8.jpg",
+        "alt_pic": "Regenie - Skin Care Product 08"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-9.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-9.jpg",
+        "alt_pic": "Regenie - Skin Care Product 09"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-10.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-10.jpg",
+        "alt_pic": "Regenie - Skin Care Product 10"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-11.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-11.jpg",
+        "alt_pic": "Regenie - Skin Care Product 11"
+    }]
+}
