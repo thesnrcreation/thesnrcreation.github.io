76ea68bcacd1ac689deca6fbd892eaf32ec89cdf
thesnrcreation <thesnrcreation@gmail.com>
2020-05-07 19:40:25 +0800

    upload commercila photo portfolio - horizon hand bag

---
+++ jsonData/photos/commercial/2018/cm-horizon-handbag.js
@@ -0,0 +1,43 @@
+var portfolio_photo = {
+    "galleryObj": [{
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-1.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-1.jpg",
+        "alt_pic": "Horizon Girl Hand Bags 01"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-2.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-2.jpg",
+        "alt_pic": "Horizon Girl Hand Bags 02"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-3.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-3.jpg",
+        "alt_pic": "Horizon Girl Hand Bags 03"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-4.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-4.jpg",
+        "alt_pic": "Horizon Girl Hand Bags 04"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-6.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-6.jpg",
+        "alt_pic": "Horizon Girl Hand Bags 06"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-7.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-7.jpg",
+        "alt_pic": "Horizon Girl Hand Bags 07"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-8.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-8.jpg",
+        "alt_pic": "Horizon Girl Hand Bags 08"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-9.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/march/horizon/portfolio/horizon-9.jpg",
+        "alt_pic": "Horizon Girl Hand Bags 09"
+    }]
+}
