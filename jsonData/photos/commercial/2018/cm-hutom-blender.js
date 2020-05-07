74da82c2546d4f75eba8fb668443e24ddeb54cee
thesnrcreation <thesnrcreation@gmail.com>
2020-05-07 21:15:11 +0800

    upload new commercial product photo portfolio - hurom blender jun 2018

---
+++ jsonData/photos/commercial/2018/cm-hutom-blender.js
@@ -0,0 +1,53 @@
+var portfolio_photo = {
+    "galleryObj": [{
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom.jpg",
+        "alt_pic": "Hurom Blender"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-1.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-1.jpg",
+        "alt_pic": "Hurom Blender 01"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-2.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-2.jpg",
+        "alt_pic": "Hurom Blender 02"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-3.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-3.jpg",
+        "alt_pic": "Hurom Blender 03"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-4.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-4.jpg",
+        "alt_pic": "Hurom Blender 04"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-6.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-6.jpg",
+        "alt_pic": "Hurom Blender 06"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-7.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-7.jpg",
+        "alt_pic": "Hurom Blender 07"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-8.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-8.jpg",
+        "alt_pic": "Hurom Blender 08"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-9.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-9.jpg",
+        "alt_pic": "Hurom Blender 09"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-10.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-10.jpg",
+        "alt_pic": "Hurom Blender 10"
+    }]
+}
