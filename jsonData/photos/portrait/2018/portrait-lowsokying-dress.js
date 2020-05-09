49b291ed8636ee99521ec35172cc968ba1bd0f9e
thesnrcreation <thesnrcreation@gmail.com>
2020-05-09 22:34:32 +0800

    add new portrait portfolio - lowsokying dress dec 2018

---
+++ jsonData/photos/portrait/2018/portrait-lowsokying-dress.js
@@ -0,0 +1,58 @@
+var portfolio_photo = {
+    "galleryObj": [{
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-1.jpg",
+        "normal_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-1.jpg",
+        "alt_pic": "Portrait - Low Sok Ying 01"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-2.jpg",
+        "normal_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-2.jpg",
+        "alt_pic": "Portrait - Low Sok Ying 02"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-3.jpg",
+        "normal_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-3.jpg",
+        "alt_pic": "Portrait - Low Sok Ying 03"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-4.jpg",
+        "normal_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-4.jpg",
+        "alt_pic": "Portrait - Low Sok Ying 04"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-5.jpg",
+        "normal_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-5.jpg",
+        "alt_pic": "Portrait - Low Sok Ying 05"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-6.jpg",
+        "normal_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-6.jpg",
+        "alt_pic": "Portrait - Low Sok Ying 06"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-7.jpg",
+        "normal_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-7.jpg",
+        "alt_pic": "Portrait - Low Sok Ying 07"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-8.jpg",
+        "normal_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-8.jpg",
+        "alt_pic": "Portrait - Low Sok Ying 08"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-9.jpg",
+        "normal_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-9.jpg",
+        "alt_pic": "Portrait - Low Sok Ying 09"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-10.jpg",
+        "normal_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-10.jpg",
+        "alt_pic": "Portrait - Low Sok Ying 10"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-11.jpg",
+        "normal_pic": "assets/images/photos/portrait/2018/dec/sokying-dress/portfolio/lsy-11.jpg",
+        "alt_pic": "Portrait - Low Sok Ying 11"
+    }]
+}
