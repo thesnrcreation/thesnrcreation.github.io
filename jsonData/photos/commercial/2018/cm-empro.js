77adc97fff09987ae357f280f8982c04f96a52c1
thesnrcreation <thesnrcreation@gmail.com>
2020-05-09 18:33:50 +0800

    upload new empro - commercial sep 2018

---
+++ jsonData/photos/commercial/2018/cm-empro.js
@@ -0,0 +1,33 @@
+var portfolio_photo = {
+    "galleryObj": [{
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/sep/empro/portfolio/lsk-1.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/sep/empro/portfolio/lsk-1.jpg",
+        "alt_pic": "Empro - Cosmetic 01"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/sep/empro/portfolio/lsk-2.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/sep/empro/portfolio/lsk-2.jpg",
+        "alt_pic": "Empro - Cosmetic 02"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/sep/empro/portfolio/lsk-3.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/sep/empro/portfolio/lsk-3.jpg",
+        "alt_pic": "Empro - Cosmetic 03"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2018/sep/empro/portfolio/lsk-4.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/sep/empro/portfolio/lsk-4.jpg",
+        "alt_pic": "Empro - Cosmetic 04"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/sep/empro/portfolio/lsk-5.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/sep/empro/portfolio/lsk-5.jpg",
+        "alt_pic": "Empro - Cosmetic 05"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2018/sep/empro/portfolio/lsk-6.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/sep/empro/portfolio/lsk-6.jpg",
+        "alt_pic": "Empro - Cosmetic 06"
+    }]
+}
