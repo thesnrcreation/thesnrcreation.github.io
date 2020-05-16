06f3e6d75b851a6436e0dabdbd4384b7f64aa45d
thesnrcreation <thesnrcreation@gmail.com>
2020-05-17 00:04:10 +0800

    add new viey portrait portfolio

---
+++ jsonData/photos/portrait/2019/portrait-viey.js
@@ -0,0 +1,28 @@
+var portfolio_photo = {
+    "galleryObj": [{
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/march/viey/portfolio/viey-1.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/march/viey/portfolio/viey-1.jpg",
+        "alt_pic": "Portrait - Viey Wong 01"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/march/viey/portfolio/viey-2.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/march/viey/portfolio/viey-2.jpg",
+        "alt_pic": "Portrait - Viey Wong 02"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/march/viey/portfolio/viey-3.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/march/viey/portfolio/viey-3.jpg",
+        "alt_pic": "Portrait - Viey Wong 03"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/march/viey/portfolio/viey-4.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/march/viey/portfolio/viey-4.jpg",
+        "alt_pic": "Portrait - Viey Wong 04"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/march/viey/portfolio/viey-5.gif",
+        "normal_pic": "assets/images/photos/portrait/2019/march/viey/portfolio/viey-5.gif",
+        "alt_pic": "Portrait - Viey Wong 05"
+    }]
+}
