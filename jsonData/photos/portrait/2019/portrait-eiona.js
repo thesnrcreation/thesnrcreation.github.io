9506394206a0c4325f05e2dd8901a549f2ca46fc
thesnrcreation <thesnrcreation@gmail.com>
2020-05-16 23:07:47 +0800

    add eiona portrait portfolio

---
+++ jsonData/photos/portrait/2019/portrait-eiona.js
@@ -0,0 +1,78 @@
+var portfolio_photo = {
+    "galleryObj": [{
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-1.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-1.jpg",
+        "alt_pic": "Portrait - Eiona 01"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-2.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-2.jpg",
+        "alt_pic": "Portrait - Eiona 02"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-3.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-3.jpg",
+        "alt_pic": "Portrait - Eiona 03"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-4.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-4.jpg",
+        "alt_pic": "Portrait - Eiona 04"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-5.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-5.jpg",
+        "alt_pic": "Portrait - Eiona 05"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-6.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-6.jpg",
+        "alt_pic": "Portrait - Eiona 06"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-7.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-7.jpg",
+        "alt_pic": "Portrait - Eiona 07"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-8.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-8.jpg",
+        "alt_pic": "Portrait - Eiona 08"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-9.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-9.jpg",
+        "alt_pic": "Portrait - Eiona 09"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-10.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-10.jpg",
+        "alt_pic": "Portrait - Eiona 10"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-11.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-11.jpg",
+        "alt_pic": "Portrait - Eiona 11"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-12.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-12.jpg",
+        "alt_pic": "Portrait - Eiona 12"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-13.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-13.jpg",
+        "alt_pic": "Portrait - Eiona 13"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-14.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-14.jpg",
+        "alt_pic": "Portrait - Eiona 14"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-15.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/june/eiona/portfolio/eiona-15.jpg",
+        "alt_pic": "Portrait - Eiona 15"
+    }]
+}
