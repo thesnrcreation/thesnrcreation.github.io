736506ca1aeb1ac3f8241075dad08546fcb17e8d
thesnrcreation <thesnrcreation@gmail.com>
2021-01-13 02:09:20 +0800

    Revert "Update v2.3"
    This reverts commit ef5319c30b3c6738f1522c8fbe56ae1e0174ae2c.

--- jsonData/photos/commercial/2019/cm-regeine-skincare.js
+++ jsonData/photos/commercial/2019/cm-regeine-skincare.js
@@ -1,63 +1,53 @@
 var portfolio_photo = {
     "galleryObj": [{
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-1.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-1.jpg",
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-1.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-1.jpg",
         "alt_pic": "Regenie - Skin Care Product 01"
     }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-2.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-2.jpg",
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-2.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-2.jpg",
         "alt_pic": "Regenie - Skin Care Product 02"
     }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-3.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-3.jpg",
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-3.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-3.jpg",
         "alt_pic": "Regenie - Skin Care Product 03"
     }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-4.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-4.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-4.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-4.jpg",
         "alt_pic": "Regenie - Skin Care Product 04"
     }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-5.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-5.jpg",
-        "alt_pic": "Regenie - Skin Care Product 05"
-    },{
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-6.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-6.jpg",
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-6.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-6.jpg",
         "alt_pic": "Regenie - Skin Care Product 06"
     }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-7.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-7.jpg",
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-7.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-7.jpg",
         "alt_pic": "Regenie - Skin Care Product 07"
     }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-8.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-8.jpg",
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-8.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-8.jpg",
         "alt_pic": "Regenie - Skin Care Product 08"
     }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-9.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-9.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-9.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-9.jpg",
         "alt_pic": "Regenie - Skin Care Product 09"
     }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-10.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-10.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-10.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-10.jpg",
         "alt_pic": "Regenie - Skin Care Product 10"
     }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-11.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-11.jpg",
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-11.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/portfolio/rg-11.jpg",
         "alt_pic": "Regenie - Skin Care Product 11"
-    }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-12.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/feb/regenie/4x5/rg-12.jpg",
-        "alt_pic": "Regenie - Skin Care Product 12"
     }]
 }
--- jsonData/photos/commercial/2019/cm-rewild.js
+++ jsonData/photos/commercial/2019/cm-rewild.js
@@ -1,63 +1,53 @@
 var portfolio_photo = {
     "galleryObj": [{
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-1.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-1.jpg",
-        "alt_pic": "Rewild - Reusable Straw 01"
-    }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-2.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-2.jpg",
-        "alt_pic": "Rewild - Reusable Straw 02"
-    }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-3.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-3.jpg",
-        "alt_pic": "Rewild - Reusable Straw 03"
-    }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-4.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-4.jpg",
-        "alt_pic": "Rewild - Reusable Straw 04"
-    }, {
-        "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-5.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-5.jpg",
-        "alt_pic": "Rewild - Reusable Straw 05"
-    }, {
-        "colSize": "col-12 col-md-4 px-4",
+        "colSize": "col-12 ",
         "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-6.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-6.jpg",
         "alt_pic": "Rewild - Reusable Straw 06"
     }, {
-        "colSize": "col-12 col-md-4 px-4",
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-2.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-2.jpg",
+        "alt_pic": "Rewild - Reusable Straw 02"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-3.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-3.jpg",
+        "alt_pic": "Rewild - Reusable Straw 03"
+    }, {
+        "colSize": "col-12 ",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-1.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-1.jpg",
+        "alt_pic": "Rewild - Reusable Straw 01"
+    }, {
+        "colSize": "col-12",
+        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-4.jpg",
+        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-4.jpg",
+        "alt_pic": "Rewild - Reusable Straw 04"
+    }, {
+        "colSize": "col-12 ",
         "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-7.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-7.jpg",
         "alt_pic": "Rewild - Reusable Straw 07"
     }, {
-        "colSize": "col-12 col-md-4 px-4",
+        "colSize": "col-12 ",
         "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-8.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-8.jpg",
         "alt_pic": "Rewild - Reusable Straw 08"
     }, {
-        "colSize": "col-12 col-md-4 px-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-9.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-9.jpg",
         "alt_pic": "Rewild - Reusable Straw 09"
     }, {
-        "colSize": "col-12 px-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-10.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-10.jpg",
         "alt_pic": "Rewild - Reusable Straw 10"
     }, {
-        "colSize": "col-12 px-4",
+        "colSize": "col-12 col-md-4",
         "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-11.jpg",
         "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-11.jpg",
         "alt_pic": "Rewild - Reusable Straw 11"
-    }, {
-        "colSize": "col-12 px-4",
-        "fullsize_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-12.jpg",
-        "normal_pic": "assets/images/photos/commercial/2019/march/rewild/portfolio/rewild-12.jpg",
-        "alt_pic": "Rewild - Reusable Straw 12"
     }]
 }
