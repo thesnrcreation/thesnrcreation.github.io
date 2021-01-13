4cfea89509d15c93ba18db2d4953b0e5e7dd6fd9
thesnrcreation <thesnrcreation@gmail.com>
2021-01-13 19:10:30 +0800

    update v2.6
    - horizon & eko cheras

--- jsonData/photos/spaces/2019/spc-ekocheras.js
+++ jsonData/photos/spaces/2019/spc-ekocheras.js
@@ -1,53 +1,63 @@
 var portfolio_photo = {
     "galleryObj": [{
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-01.jpg",
-        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-01.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-01.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-01.jpg",
         "alt_pic": "Interior - Eko Cheras 01"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-02.jpg",
-        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-02.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-02.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-02.jpg",
         "alt_pic": "Interior - Eko Cheras 02"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-03.jpg",
-        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-03.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-03.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-03.jpg",
         "alt_pic": "Interior - Eko Cheras 03"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-04.jpg",
-        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-04.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-04.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-04.jpg",
         "alt_pic": "Interior - Eko Cheras 04"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-05.jpg",
-        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-05.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-05.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-05.jpg",
         "alt_pic": "Interior - Eko Cheras 05"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-06.jpg",
-        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-06.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-06.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-06.jpg",
         "alt_pic": "Interior - Eko Cheras 06"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-07.jpg",
-        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-07.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-07.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-07.jpg",
         "alt_pic": "Interior - Eko Cheras 07"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-08.jpg",
-        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-08.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-08.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-08.jpg",
         "alt_pic": "Interior - Eko Cheras 08"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-09.jpg",
-        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-09.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-09.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-09.jpg",
         "alt_pic": "Interior - Eko Cheras 09"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-10.jpg",
-        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/portfolio/eko-10.jpg",
+        "colSize": "col-12 px-4",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-10.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-10.jpg",
         "alt_pic": "Interior - Eko Cheras 10"
+    }, {
+        "colSize": "col-12 px-4",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-11.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-11.jpg",
+        "alt_pic": "Interior - Eko Cheras 11"
+    }, {
+        "colSize": "col-12 px-4",
+        "fullsize_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-12.jpg",
+        "normal_pic": "assets/images/photos/spaces/2019/ekocheras/4x5/eko-12.jpg",
+        "alt_pic": "Interior - Eko Cheras 12"
     }]
 }
