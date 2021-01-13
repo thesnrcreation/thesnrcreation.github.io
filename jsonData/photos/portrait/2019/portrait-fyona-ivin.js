06ffdf4e19a9b05013501686b7a0965ceab2ff19
thesnrcreation <thesnrcreation@gmail.com>
2021-01-14 01:43:43 +0800

    update 2.9 ivin n fyona

--- jsonData/photos/portrait/2019/portrait-fyona-ivin.js
+++ jsonData/photos/portrait/2019/portrait-fyona-ivin.js
@@ -1,73 +1,78 @@
 var portfolio_photo = {
     "galleryObj": [{
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-1.jpg",
-        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-1.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-01.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-01.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 01"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-2.jpg",
-        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-2.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-02.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-02.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 02"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-3.jpg",
-        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-3.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-03.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-03.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 03"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-4.jpg",
-        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-4.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-04.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-04.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 04"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-5.jpg",
-        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-5.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-05.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-05.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 05"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-6.jpg",
-        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-6.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-06.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-06.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 06"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-7.jpg",
-        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-7.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-07.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-07.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 07"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-8.jpg",
-        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-8.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-08.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-08.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 08"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-9.jpg",
-        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-9.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-09.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-09.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 09"
     }, {
-        "colSize": "col-12",
+        "colSize": "col-12 col-md-4 px-4",
         "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-10.jpg",
         "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-10.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 10"
     }, {
-        "colSize": "col-12",
+        "colSize": "col-12 col-md-4 px-4",
         "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-11.jpg",
         "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-11.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 11"
     }, {
-        "colSize": "col-12",
+        "colSize": "col-12 col-md-4 px-4",
         "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-12.jpg",
         "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-12.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 12"
     }, {
-        "colSize": "col-12",
+        "colSize": "col-12 col-md-4 px-4",
         "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-13.jpg",
         "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-13.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 13"
     }, {
-        "colSize": "col-12",
+        "colSize": "col-12 col-md-4 px-4",
         "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-14.jpg",
         "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-14.jpg",
         "alt_pic": "Portrait - Fyona & Ivin 14"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-15.jpg",
+        "normal_pic": "assets/images/photos/portrait/2019/sep/ivin-fyona/portfolio/fyiv-15.jpg",
+        "alt_pic": "Portrait - Fyona & Ivin 15"
     }]
 }
