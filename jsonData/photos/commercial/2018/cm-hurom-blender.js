9c4177787e97084ec6f98d1c1f41bf0dd3881a00
thesnrcreation <thesnrcreation@gmail.com>
2021-01-13 18:40:50 +0800

    Update v2.5
    - Commercial Hurom

--- jsonData/photos/commercial/2018/cm-hurom-blender.js
+++ jsonData/photos/commercial/2018/cm-hurom-blender.js
@@ -1,53 +1,53 @@
 var portfolio_photo = {
     "galleryObj": [{
-        "colSize": "col-12 ",
-        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom.jpg",
-        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom.jpg",
-        "alt_pic": "Hurom Blender"
-    }, {
-        "colSize": "col-12 ",
-        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-1.jpg",
-        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-1.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-1.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-1.jpg",
         "alt_pic": "Hurom Blender 01"
     }, {
-        "colSize": "col-12 ",
-        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-2.jpg",
-        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-2.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-2.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-2.jpg",
         "alt_pic": "Hurom Blender 02"
     }, {
-        "colSize": "col-12 ",
-        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-3.jpg",
-        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-3.jpg",
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-3.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-3.jpg",
         "alt_pic": "Hurom Blender 03"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-4.jpg",
-        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-4.jpg",
+        "colSize": "col-12 px-4",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-4.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-4.jpg",
         "alt_pic": "Hurom Blender 04"
     }, {
-        "colSize": "col-12 ",
-        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-6.jpg",
-        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-6.jpg",
-        "alt_pic": "Hurom Blender 06"
+        "colSize": "col-12 px-4",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-5.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-5.jpg",
+        "alt_pic": "Hurom Blender 05"
     }, {
-        "colSize": "col-12 ",
-        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-7.jpg",
-        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-7.jpg",
+        "colSize": "col-12 px-4",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-7.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-7.jpg",
         "alt_pic": "Hurom Blender 07"
     }, {
-        "colSize": "col-12 ",
-        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-8.jpg",
-        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-8.jpg",
+        "colSize": "col-12 px-4",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-8.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-8.jpg",
         "alt_pic": "Hurom Blender 08"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-9.jpg",
-        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-9.jpg",
+        "colSize": "col-12 px-4",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-9.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-9.jpg",
         "alt_pic": "Hurom Blender 09"
     }, {
-        "colSize": "col-12",
-        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-10.jpg",
-        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/portfolio/hurom-10.jpg",
+        "colSize": "col-12 px-4",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-10.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-10.jpg",
         "alt_pic": "Hurom Blender 10"
+    }, {
+        "colSize": "col-12 px-4",
+        "fullsize_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-11.jpg",
+        "normal_pic": "assets/images/photos/commercial/2018/jun/hurom/4x5/h-11.jpg",
+        "alt_pic": "Hurom Blender 11"
     }]
 }
