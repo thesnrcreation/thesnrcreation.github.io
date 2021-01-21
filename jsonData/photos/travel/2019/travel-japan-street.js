49df3e099b1fe907bf7afce0ad6a52206ee636d5
thesnrcreation <thesnrcreation@gmail.com>
2021-01-21 17:39:38 +0800

    update 3.9 - upload japan street porfolio

--- jsonData/photos/travel/2019/travel-japan-street.js
+++ jsonData/photos/travel/2019/travel-japan-street.js
@@ -1,168 +1,213 @@
 var portfolio_photo = {
     "galleryObj": [{
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-01.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-01.jpg",
-        "alt_pic": "Japan - Osaka 01"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-1.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-1.jpg",
+        "alt_pic": "Japan - Street 01"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-02.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-02.jpg",
-        "alt_pic": "Japan - Osaka 02"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-2.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-2.jpg",
+        "alt_pic": "Japan - Street 02"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-03.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-03.jpg",
-        "alt_pic": "Japan - Osaka 03"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-3.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-3.jpg",
+        "alt_pic": "Japan - Street 03"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-04.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-04.jpg",
-        "alt_pic": "Japan - Osaka 04"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-4.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-4.jpg",
+        "alt_pic": "Japan - Street 04"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-05.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-05.jpg",
-        "alt_pic": "Japan - Osaka 05"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-5.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-5.jpg",
+        "alt_pic": "Japan - Street 05"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-06.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-06.jpg",
-        "alt_pic": "Japan - Osaka 06"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-6.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-6.jpg",
+        "alt_pic": "Japan - Street 06"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-07.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-07.jpg",
-        "alt_pic": "Japan - Osaka 07"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-7.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-7.jpg",
+        "alt_pic": "Japan - Street 07"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-08.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-08.jpg",
-        "alt_pic": "Japan - Osaka 08"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-8.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-8.jpg",
+        "alt_pic": "Japan - Street 08"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-09.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-09.jpg",
-        "alt_pic": "Japan - Osaka 09"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-9.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-9.jpg",
+        "alt_pic": "Japan - Street 09"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-10.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-10.jpg",
-        "alt_pic": "Japan - Osaka 10"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-10.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-10.jpg",
+        "alt_pic": "Japan - Street 10"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-11.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-11.jpg",
-        "alt_pic": "Japan - Osaka 11"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-11.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-11.jpg",
+        "alt_pic": "Japan - Street 11"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-12.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-12.jpg",
-        "alt_pic": "Japan - Osaka 12"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-12.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-12.jpg",
+        "alt_pic": "Japan - Street 12"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-13.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-13.jpg",
-        "alt_pic": "Japan - Osaka 13"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-13.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-13.jpg",
+        "alt_pic": "Japan - Street 13"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-14.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-14.jpg",
-        "alt_pic": "Japan - Osaka 14"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-14.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-14.jpg",
+        "alt_pic": "Japan - Street 14"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-15.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-15.jpg",
-        "alt_pic": "Japan - Osaka 15"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-15.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-15.jpg",
+        "alt_pic": "Japan - Street 15"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-16.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-16.jpg",
-        "alt_pic": "Japan - Osaka 16"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-16.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-16.jpg",
+        "alt_pic": "Japan - Street 16"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-17.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-17.jpg",
-        "alt_pic": "Japan - Osaka 17"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-17.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-17.jpg",
+        "alt_pic": "Japan - Street 17"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-18.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-18.jpg",
-        "alt_pic": "Japan - Osaka 18"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-18.png",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-18.png",
+        "alt_pic": "Japan - Street 18"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-19.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-19.jpg",
-        "alt_pic": "Japan - Osaka 19"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-19.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-19.jpg",
+        "alt_pic": "Japan - Street 19"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-20.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-20.jpg",
-        "alt_pic": "Japan - Osaka 20"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-20.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-20.jpg",
+        "alt_pic": "Japan - Street 20"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-21.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-21.jpg",
-        "alt_pic": "Japan - Osaka 21"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-21.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-21.jpg",
+        "alt_pic": "Japan - Street 21"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-22.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-22.jpg",
-        "alt_pic": "Japan - Osaka 22"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-22.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-22.jpg",
+        "alt_pic": "Japan - Street 22"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-23.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-23.jpg",
-        "alt_pic": "Japan - Osaka 23"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-23.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-23.jpg",
+        "alt_pic": "Japan - Street 23"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-24.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-24.jpg",
-        "alt_pic": "Japan - Osaka 24"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-24.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-24.jpg",
+        "alt_pic": "Japan - Street 24"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-25.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-25.jpg",
-        "alt_pic": "Japan - Osaka 25"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-25.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-25.jpg",
+        "alt_pic": "Japan - Street 25"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-26.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-26.jpg",
-        "alt_pic": "Japan - Osaka 25"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-26.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-26.jpg",
+        "alt_pic": "Japan - Street 25"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-27.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-27.jpg",
-        "alt_pic": "Japan - Osaka 27"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-27.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-27.jpg",
+        "alt_pic": "Japan - Street 27"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-28.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-28.jpg",
-        "alt_pic": "Japan - Osaka 28"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-28.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-28.jpg",
+        "alt_pic": "Japan - Street 28"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-29.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-29.jpg",
-        "alt_pic": "Japan - Osaka 29"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-29.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-29.jpg",
+        "alt_pic": "Japan - Street 29"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-30.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-30.jpg",
-        "alt_pic": "Japan - Osaka 30"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-30.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-30.jpg",
+        "alt_pic": "Japan - Street 30"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-31.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-31.jpg",
-        "alt_pic": "Japan - Osaka 31"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-31.png",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-31.png",
+        "alt_pic": "Japan - Street 31"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-32.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-32.jpg",
-        "alt_pic": "Japan - Osaka 32"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-32.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-32.jpg",
+        "alt_pic": "Japan - Street 32"
     }, {
         "colSize": "col-12 col-md-4 px-4",
-        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-33.jpg",
-        "normal_pic": "assets/images/photos/travel/2019/sep/japan-osaka/4x5/osaka-33.jpg",
-        "alt_pic": "Japan - Osaka 33"
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-33.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-33.jpg",
+        "alt_pic": "Japan - Street 33"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-34.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-34.jpg",
+        "alt_pic": "Japan - Street 34"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-35.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-35.jpg",
+        "alt_pic": "Japan - Street 35"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-36.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-36.jpg",
+        "alt_pic": "Japan - Street 36"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-37.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-37.jpg",
+        "alt_pic": "Japan - Street 37"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-38.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-38.jpg",
+        "alt_pic": "Japan - Street 38"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-39.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-39.jpg",
+        "alt_pic": "Japan - Street 39"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-40.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-40.jpg",
+        "alt_pic": "Japan - Street 40"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-41.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-41.jpg",
+        "alt_pic": "Japan - Street 41"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-42.jpg",
+        "normal_pic": "assets/images/photos/travel/2019/sep/japan-street/4x5/street-42.jpg",
+        "alt_pic": "Japan - Street 42"
     }]
 }
