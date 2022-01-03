48d26af546080e92360a2c2b8323f08674420b68
thesnrcreation <thesnrcreation@gmail.com>
2022-01-03 21:59:03 +0800

    upload ivin portfolio

---
+++ jsonData/photos/portrait/2021/portrait-ivin.js
@@ -0,0 +1,133 @@
+var portfolio_photo = {
+    "galleryObj": [{
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-01.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-01.jpg",
+        "alt_pic": "Portrait - Ivin Tan 01"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-02.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-02.jpg",
+        "alt_pic": "Portrait - Ivin Tan 02"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-03.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-03.jpg",
+        "alt_pic": "Portrait - Ivin Tan 03"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-04.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-04.jpg",
+        "alt_pic": "Portrait - Ivin Tan 04"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-05.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-05.jpg",
+        "alt_pic": "Portrait - Ivin Tan 05"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-06.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-06.jpg",
+        "alt_pic": "Portrait - Ivin Tan 06"
+    }, {
+        "colSize": "col-12 col-md-12 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-07.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-07.jpg",
+        "alt_pic": "Portrait - Ivin Tan 07"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-08.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-08.jpg",
+        "alt_pic": "Portrait - Ivin Tan 08"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-09.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-09.jpg",
+        "alt_pic": "Portrait - Ivin Tan 09"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-10.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-10.jpg",
+        "alt_pic": "Portrait - Ivin Tan 10"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-11.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-11.jpg",
+        "alt_pic": "Portrait - Ivin Tan 11"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-12.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-12.jpg",
+        "alt_pic": "Portrait - Ivin Tan 12"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-13.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-13.jpg",
+        "alt_pic": "Portrait - Ivin Tan 13"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-14.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-14.jpg",
+        "alt_pic": "Portrait - Ivin Tan 14"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-15.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-15.jpg",
+        "alt_pic": "Portrait - Ivin Tan 15"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-16.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-16.jpg",
+        "alt_pic": "Portrait - Ivin Tan 16"
+    }, {
+        "colSize": "col-12 col-md-12 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-17.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-17.jpg",
+        "alt_pic": "Portrait - Ivin Tan 17"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-18.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-18.jpg",
+        "alt_pic": "Portrait - Ivin Tan 18"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-19.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-19.jpg",
+        "alt_pic": "Portrait - Ivin Tan 19"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-20.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-20.jpg",
+        "alt_pic": "Portrait - Ivin Tan 20"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-21.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-21.jpg",
+        "alt_pic": "Portrait - Ivin Tan 21"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-22.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-22.jpg",
+        "alt_pic": "Portrait - Ivin Tan 22"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-23.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-23.jpg",
+        "alt_pic": "Portrait - Ivin Tan 23"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-24.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-24.jpg",
+        "alt_pic": "Portrait - Ivin Tan 24"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-25.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-25.jpg",
+        "alt_pic": "Portrait - Ivin Tan 25"
+    }, {
+        "colSize": "col-12 col-md-4 px-4",
+        "fullsize_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-26.jpg",
+        "normal_pic": "../assets/images/photos/portrait/2021/nov/ivin/4x5/ivin-26.jpg",
+        "alt_pic": "Portrait - Ivin Tan 26"
+    }]
+}
